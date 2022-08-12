aa71223f4218d4b1385f9e1381dcea79e66560a9
Rcoronetti <rafaelcoronetti@gmail.com>
2022-08-12 14:03:32 -0300

    adicionando arquivos de aulas

---
+++ 2_FUNDAMENTOS/5_modulos_externos/index.mjs
@@ -0,0 +1,10 @@
+//minimist possibilita pegar argumentos e extrai=los diretamente da linha de comando.
+//Nesse exemplo estamos pegando o nome e a profissão, que são repassadas diretamente na linha de comando => node index.mjs --nome=Rafael --profissao=Programador.
+import minimist from 'minimist';
+const args = minimist(process.argv.slice(2));
+console.log(args);
+
+const nome = args["nome"]
+const profissao = args['profissao']
+console.log(nome, profissao);
+console.log(`O nome dele é ${nome} e sua profissão é ${profissao}`);
