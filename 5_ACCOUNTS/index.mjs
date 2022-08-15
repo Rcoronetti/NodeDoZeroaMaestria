9ef0de4f3c1b92717b90c529e2f9a8d8bf8c9633
Rcoronetti <rafaelcoronetti@gmail.com>
2022-08-15 15:14:54 -0300

    inicio projeto account

---
+++ 5_ACCOUNTS/index.mjs
@@ -0,0 +1,131 @@
+//modulos externos
+import inquirer from "inquirer";
+import chalk from 'chalk'
+
+//modulos internos
+import fs from 'fs'
+
+operation()
+
+function operation() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        choices: [
+            'Criar conta',
+            'Consultar saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ],
+    },
+        //esse then seria a solução das escolhas do usuário. 
+        //recebemos uma resposta(answer) e criamos uma função baseada na resposta.
+    ]).then((answer) => {
+        //pegar a ação do usuário com [action], por q é um array de perguntas então teremos um array de respostas.
+        const action = answer['action']
+
+        //se a ação for criar conta, então entramos na função criar conta.
+        if (action === 'Criar conta') {
+            createAccount()
+
+        } else if (action === 'Depositar') {
+            deposit()
+
+
+        } else if (action === 'Consultar saldo') {
+
+
+        } else if (action === 'Sacar') {
+
+
+        } else if (action === 'Sair') {
+            console.log(chalk.bgBlue.black('Obrigado por usar o Accounts!'))
+            process.exit()
+
+        }
+
+        //aqui vamos ver a escolha do usuário.
+        //console.log(action);
+    })
+        // o catch para pegarmos um eventual erro..
+        .catch(err => console.log(err))
+}
+
+//create an account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso Banco!'))
+    console.log(chalk.green('Defina as opçoes da sua conta a seguir'))
+
+    buildAccount()
+    return
+}
+//construindo a conta
+function buildAccount() {
+    inquirer.prompt([{
+        name: 'accountName',
+        message: 'digite um nome para sua conta: '
+
+        //recebendo a resposta.
+    }]).then((answer) => {
+        const accountName = answer['accountName']
+        console.info(accountName)
+
+        //Criar banco de dados das contas
+        //verificar se existe diretório, caso contrário criar.
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+        //validação de criação de conta, verificar se a mesma já está criada.
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'))
+
+            //inicia novo ciclo na criação da conta, o usuário vai escolher novo nome para conta.
+            buildAccount()
+            return
+        }// escrevendo arquivo já criado e dando um valor para o mesmo, no caso balance 0
+        fs.writeFileSync(
+            `accounts/${accountName}.json`,
+            '{ "balance": 0 }',
+
+            //evidenciando erro se tiver.
+            function (err) {
+                console.log((err))
+            })
+
+        console.log(chalk.green('Parabéns a sua conta foi criada!'));
+        operation()
+
+    })
+        .catch(err => console.log(err))
+
+}
+
+//add an amount to user account
+function deposit() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual o nome da sua conta?'
+        }
+    ])
+        .then((answer) => {
+            const accountName = answer['accountName']
+
+            // verificando se conta existe
+            if (!checkAccount(accountName)) {
+                return deposit()
+            }
+        })
+        .catch(err => console.log(err))
+}
+
+
+//verifica se a conta existe
+function checkAccount(accountName) {
+    if (!fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(chalk.bgRed.black('Esta conta não existe... tente novamente!'))
+        return false
+    }
+    return true
+}
