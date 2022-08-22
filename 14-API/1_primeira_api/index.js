b828ad02c8dd6a9528f1eda7ea2265c8c293aad3
Rcoronetti <rafaelcoronetti@gmail.com>
2022-08-22 16:50:19 -0300

    fim do projeto

---
+++ 14-API/1_primeira_api/index.js
@@ -0,0 +1,32 @@
+// acessar http://localhost:3000/createproduct
+import express from 'express'
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+app.post('/createproduct', (req, res) => {
+    const name = req.body.name
+    const price = req.body.price
+
+    if (!name) {
+        res.status(422).json({ message: 'O campo nome é obrigatório!' })
+        return
+    }
+
+    console.log(name)
+    console.log(price)
+
+    res.status(201).json({ message: `O produto: ${name} com preço ${price} foi criado com sucesso!` })
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Primeira rota criada com sucesso!' })
+})
+
+app.listen(3000)
